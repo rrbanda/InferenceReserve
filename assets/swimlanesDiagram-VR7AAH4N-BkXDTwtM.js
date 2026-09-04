de66d7e31554c580d66b0db0cd508ed4903d3240
Raghu Banda <rrbanda@users.noreply.github.com>
2026-09-04 00:37:35 -0400

    Updates

---
+++ assets/swimlanesDiagram-VR7AAH4N-BkXDTwtM.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-HODETNUW-CfmlzJYo.js";import{_ as a}from"./index-s82H36ZU.js";import"./chunk-5VM5RSS4-B30uEi_g.js";import"./chunk-XXDRQBXY-ChxM5uw2.js";import"./chunk-POPQ4Y6H-BHRrWYe0.js";import"./chunk-F27PBJKO-BgkdA09P.js";import"./channel-CeARmoAm.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
